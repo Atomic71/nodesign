01d07f98af1bc93ab2e6d4dd9e47fa9860cd7c67
Matrixdjeva <bojanacucak8796@gmail.com>
2024-09-09 10:33:29 +0200

    Inicijalna verzija

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './src/**/*.{html,js,astro}', // Dodaj putanje do svojih fajlova
+    './public/**/*.html', 
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
